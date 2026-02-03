5dccd1c952bf75bde2abfb1172324a998cf4c223
aryan29gupta <aryangupta2906@gmail.com>
2026-02-03 22:27:02 +0530

    Ques 75

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ All solutions are written with a focus on **clear logic, clean code, and strong 
 | 12 | 136 | Single Number | Arrays / Bit Manipulation | O(n) | O(1) |
 | 13 | 189 | Rotate Array | Arrays | O(n) | O(1) |
 | 14 | 283 | Move Zeroes | Arrays / Two Pointers | O(n) | O(1) |
+| 15 | 75 | Sort Colors | Arrays / Two Pointers (Dutch National Flag) | O(n) | O(1) |
 
 > ✅ Table will be updated regularly as I solve more problems.
 
---
+++ array/75_sort_colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        int temp;
+        while (mid<=high){
+            if(nums[mid]==0){
+                temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
+    }
+};
